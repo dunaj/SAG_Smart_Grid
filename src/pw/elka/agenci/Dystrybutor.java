9c726019c6b94046c1f494dd7fdce4f90ceeae0c
dunaj <adam.doonay@gmail.com>
2014-05-30 15:43:54 +0200

    Skończony Odbiorca, 1/2 Dystrybutora

--- src/pw/elka/agenci/Dystrybutor.java
+++ src/pw/elka/agenci/Dystrybutor.java
@@ -1,5 +1,6 @@
 package pw.elka.agenci;
 
+import jade.core.AID;
 import jade.core.Agent;
 import jade.core.behaviours.Behaviour;
 import jade.core.behaviours.CyclicBehaviour;
@@ -9,19 +10,17 @@ import jade.lang.acl.MessageTemplate;
 import java.util.Vector;
 
 /**
- * Agent odpowiadajacy za odbieranie prosb o energie
- * od odbiorcow i przekazywanie prosb do Elektrowni,
- * a takze za poszukiwanie brakujacej energii u innych
- * Dystrybutorow, wreszcie przekazywanie energii
- * do odbiorcow.
+ * Agent odpowiadajacy za odbieranie prosb o energie od odbiorcow i
+ * przekazywanie prosb do Elektrowni, a takze za poszukiwanie brakujacej energii
+ * u innych Dystrybutorow, wreszcie przekazywanie energii do odbiorcow.
  */
 public class Dystrybutor extends Agent implements OdbieraczEnergii {
 
 	private static final long serialVersionUID = 8713713221778399107L;
-	
+
 	private Elektrownia elektrownia;
 	private Vector<Dystrybutor> dytrybutorzy;
-	
+
 	@Override
 	protected void setup() {
 		// TODO Auto-generated method stub
@@ -33,28 +32,27 @@ public class Dystrybutor extends Agent implements OdbieraczEnergii {
 		// TODO Auto-generated method stub
 		super.takeDown();
 	}
-	
+
 	/**
-	 * Wewn klasa implementujaca zachowanie Dystrybutorow polegajace na zbieraniu prosb
-	 * dotyczacych zapotrzebowania na energie od odbiorcow
-	 * TODO czy ta klasa jest potrzebna?
+	 * Wewn klasa implementujaca zachowanie Dystrybutorow polegajace na
+	 * zbieraniu prosb dotyczacych zapotrzebowania na energie od odbiorcow TODO
+	 * czy ta klasa jest potrzebna?
 	 */
 	public class ZbieranieProsb extends CyclicBehaviour {
 
 		private static final long serialVersionUID = -8694733170063523008L;
 
-		/**
-		 * TODO skonczyc ta metode
-		 */
 		@Override
 		public void action() {
 			// Dystrybutor oczekuje wiadomosci typu: prosba w tym zachowaniu
 			MessageTemplate mt = MessageTemplate
 					.MatchPerformative(ACLMessage.REQUEST);
-			ACLMessage msg = myAgent.receive(mt);
-			if (msg != null) {
-				// Message received. Process it
-				// dodaj pojedyncze zachowanie Szukaj Energii (ilosc)
+			ACLMessage prosba = myAgent.receive(mt);
+			if (prosba != null) {
+				// jesli jest
+				AID odbiorca = prosba.getSender();
+				int energia = Integer.parseInt((prosba.getContent()));
+				myAgent.addBehaviour(new SzukajEnergii(odbiorca, energia));
 			} else {
 				// jesli nie otrzymalem wiadomosci, to blokuje watek
 				block();
@@ -62,95 +60,103 @@ public class Dystrybutor extends Agent implements OdbieraczEnergii {
 		}
 
 	}
-	
+
 	/**
-	 * Klasa implementujaca zachowanie Dystrybutora
-	 * polegajace na poszukiwaniu energii w sieci.
-	 * Dystrybutor najpierw szuka energii 
-	 * u swojej Elektrowni, potem pyta Dystrybutorow
+	 * Klasa implementujaca zachowanie Dystrybutora polegajace na poszukiwaniu
+	 * energii w sieci. Dystrybutor najpierw szuka energii u swojej Elektrowni,
+	 * potem pyta Dystrybutorow
 	 * 
 	 * TODO czy na pewno takie dzialanie wyszukiwania
 	 */
 	public class SzukajEnergii extends Behaviour {
 
 		private static final long serialVersionUID = -7619324663836444757L;
-		
+
 		private int szukanaEnergia;
 		private int krok = 0;
-		
-		public SzukajEnergii (int energia) {
-			szukanaEnergia = energia;
+		private AID odbiorca;
+
+		public SzukajEnergii(AID odbiorca, int energia) {
+			this.odbiorca = odbiorca;
+			this.szukanaEnergia = energia;
 		}
-		
+
 		@Override
 		public void action() {
 			MessageTemplate mtAgree = MessageTemplate
 					.MatchPerformative(ACLMessage.AGREE);
 			MessageTemplate mtRefuse = MessageTemplate
 					.MatchPerformative(ACLMessage.REFUSE);
-			//1. pytaj swojej elektrowni (swoich elektrownii) czy ma
-			//2. jesli otrzymasz odpowiedz ze elektrownia ma energie ()
-			//			dodaj zachowanie dostarczenia z otrzymana energia
-			//	 w pp:
-			//			szukaj u dystrybutorow polaczonych z Toba
-			//3. jak otrzymasz potwierdzenie od dystrybutora
-			//		wiadomosc do wszyskich, ze juz masz
-			//4. dodaj zachowanie dostarczenia z otrzymana energia
+			// 1. pytaj swojej elektrowni (swoich elektrownii) czy ma
+			// 2. jesli otrzymasz odpowiedz ze elektrownia ma energie ()
+			// dodaj zachowanie dostarczenia z otrzymana energia
+			// w pp:
+			// szukaj u dystrybutorow polaczonych z Toba
+			// 3. jak otrzymasz potwierdzenie od dystrybutora
+			// wiadomosc do wszyskich, ze juz masz
+			// 4. dodaj zachowanie dostarczenia z otrzymana energia
 			switch (krok) {
 			case 0: // pytaj swojej elektrowni
 				ACLMessage prosba = new ACLMessage(ACLMessage.REQUEST);
 				prosba.addReceiver(elektrownia.getAID());
-				prosba.setContent("Energia");
-				prosba.setPerformative(szukanaEnergia);
+				prosba.setContent(String.valueOf(szukanaEnergia));
+				prosba.setPerformative(ACLMessage.REQUEST);
 				myAgent.send(prosba);
-				krok=1;
+				krok = 1;
 				break;
 			case 1:
-				ACLMessage odp = myAgent.receive(mtAgree); 
-				if (odp != null) { 
-					 // pozytywna odpowiedz odebrana
-						 //Elektrownia ma tyle energii 
-						 // TODO stworzyc zadanie dostarczenia energii
-						 // TODO ustawic krok na wartosc koncowa 
-				 }
-				 else {
-					odp = myAgent.receive(mtRefuse);
-					if (odp != null) {
-					//negatywna odpowiedz
-					//TODO poszukiwanie energii u innych dystrybutorow
-					}
-				 } 
+				ACLMessage zgoda = myAgent.receive(mtAgree);
+				ACLMessage odmowa = myAgent.receive(mtRefuse);
+				if (zgoda != null) {
+					// pozytywna odpowiedz - Elektrownia ma tyle energii
+					myAgent.addBehaviour(new DostarczanieEnergii(odbiorca,
+							szukanaEnergia));
+					// koncz szukanie energii
+					krok = 3;
+				} else if (odmowa != null) {
+
+					// negatywna odpowiedz
+					// TODO poszukiwanie energii u innych dystrybutorow
+					krok = 2;
+
+				} else {
+					block();
+				}
 				break;
 			case 2:
-				break;
-			case 3:
+				ACLMessage odp2 = myAgent.receive(mtAgree);
+				if (odp2 != null) {
+					// TODO wyslij broadcast ze juz masz
+					// TODO stworzyc zadanie dostarczenia energii
+				}
+				krok = 3;
 				break;
 			}
-			
+
 		}
 
 		@Override
 		public boolean done() {
-			// TODO Auto-generated method stub
-			return false;
+			return (krok == 3);
 		}
 
 	}
-	
+
 	/**
-	 * Klasa implementujaca zachowania dostarczania 
-	 * energii przez dystrybutorow
+	 * Klasa implementujaca zachowania dostarczania energii przez dystrybutorow
 	 */
 	public class DostarczanieEnergii extends Behaviour {
 
 		private static final long serialVersionUID = 2891929086673214889L;
-		
+
 		private int dostarczanaEnergia;
-		
-		public DostarczanieEnergii(int energia) {
+		private AID odbiorca;
+
+		public DostarczanieEnergii(AID odbiorca, int energia) {
+			this.odbiorca = odbiorca;
 			dostarczanaEnergia = energia;
 		}
-		
+
 		@Override
 		public void action() {
 			// TODO Auto-generated method stub
--- src/pw/elka/agenci/Odbiorca.java
+++ src/pw/elka/agenci/Odbiorca.java
@@ -1,31 +1,129 @@
 package pw.elka.agenci;
 
 import jade.core.Agent;
+import jade.core.behaviours.Behaviour;
 import jade.core.behaviours.CyclicBehaviour;
+import jade.core.behaviours.TickerBehaviour;
+import jade.lang.acl.ACLMessage;
+import jade.lang.acl.MessageTemplate;
+
+import java.util.Random;
 
 /**
- * Agent proszacy o energie dytrybutorow
- * i wykorzystujacy energie.
+ * Agent proszacy o energie dytrybutorow i wykorzystujacy energie.
  */
 public class Odbiorca extends Agent implements OdbieraczEnergii {
 
 	private static final long serialVersionUID = -7698025950501425520L;
+	private static final int CZAS_TIKA = 2000;
+
+	private Dystrybutor dystrybutor;
+	/**
+	 * pole pokazujace ile odbiorca potrzebuje pradu
+	 */
+	private int zapotrzebowanie = 0;
 
 	/**
-	 * Wewn klasa implementujaca zachowanie Odbiorcow
-	 * Cykliczny pobor pradu.
-	 * TODO zdecydowac czy to powinno byc rzeczywiscie Cykliczne czy moze
-	 *  	Tick Behaviour
+	 * Konstruktor przyjmujacy Dystrybutora z ktorym ma byc polaczony Odbiorca
+	 * 
+	 * @param d
+	 *            - dystrybutor, z ktorym ma byc polaczony Odbiorca
 	 */
-	public class PoborPradu extends CyclicBehaviour {
+	public Odbiorca(Dystrybutor d) {
+		this.dystrybutor = d;
+	}
+
+	@Override
+	protected void setup() {
+		super.setup();
+		addBehaviour(new TickerBehaviour(this, CZAS_TIKA) {
+			/**
+			 * 
+			 */
+			private static final long serialVersionUID = 6022385825163721857L;
+
+			protected void onTick() {
+				addBehaviour(new PoborPradu());
+			}
+		});
+		addBehaviour(new OdbiorPradu());
+	}
+
+	/**
+	 * Generator poboru pradu w danej chwili.
+	 * 
+	 * @return losowa ilosc energii
+	 */
+	private int ileEnergii() {
+		Random gen = new Random();
+		return gen.nextInt(1000);
+	}
+
+	/**
+	 * funkcja zwiekszajaca zapotrzebowanie na prad, wywolywana po wyslaniu
+	 * prosby o energie do Dystrybutora
+	 */
+	private void zwiekszZapotrzebowanie(int ile) {
+		zapotrzebowanie += ile;
+	}
+
+	/**
+	 * funkcja zmniejszajaca zapotrzebowanie na prad, wywolywana po odebraniu
+	 * energii od Dystrybutora
+	 */
+	private void zmniejszZapotrzebowanie(int ile) {
+		zapotrzebowanie -= ile;
+	}
+	
+	/**
+	 * funkcja wyswietlajaca aktualne zapotrzebowanie na energie
+	 */
+	public String jakieZapotrzebowanie() {
+		return "Zapotrzebowanie Odbiorcy: "+zapotrzebowanie;
+	}
+	
+	/**
+	 * Wewn klasa implementujaca zachowanie Odbiorcow Cykliczny pobor pradu.
+	 */
+	public class PoborPradu extends Behaviour {
 
 		private static final long serialVersionUID = -6705750061317380494L;
 
 		@Override
 		public void action() {
-			// TODO Auto-generated method stub
 
+			ACLMessage prosba = new ACLMessage(ACLMessage.REQUEST);
+			int ile = ileEnergii();
+			prosba.setContent(String.valueOf(ile));
+			prosba.addReceiver(dystrybutor.getAID());
+			((Odbiorca) myAgent).zwiekszZapotrzebowanie(ile);
+			myAgent.send(prosba);
 		}
 
+		@Override
+		public boolean done() {
+			return true;
+		}
+
+	}
+
+	/**
+	 * Zachowanie odpowiedzialne za odbior pradu od Dystrybutora
+	 */
+	public class OdbiorPradu extends CyclicBehaviour {
+
+		private static final long serialVersionUID = 6753410205530977115L;
+
+		@Override
+		public void action() {
+			MessageTemplate mtAgree = MessageTemplate
+					.MatchPerformative(ACLMessage.AGREE);
+			ACLMessage odp = myAgent.receive(mtAgree);
+			if (odp != null) {
+				zmniejszZapotrzebowanie(Integer.parseInt(odp.getContent()));
+			} else {
+				block();
+			}
+		}
 	}
 }
