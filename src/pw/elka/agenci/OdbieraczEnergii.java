4ab58fb9f1738898caca4b299a1fa8bc8135c9f1
dunaj <adam.doonay@gmail.com>
2014-05-30 13:20:46 +0200

    Odbieracz

---
+++ src/pw/elka/agenci/OdbieraczEnergii.java
@@ -0,0 +1,10 @@
+package pw.elka.agenci;
+
+/**
+ * Interfejs, ktory bedzie implementowany przez
+ * klasy: Odbiorca i Dystrybutor
+ * czyli te, ktore beda musialy odbierac energie
+ */
+public interface OdbieraczEnergii {
+
+}
